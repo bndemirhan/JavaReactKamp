ecac94523683fda6dced38b33023cb4e349593d2
Betül Nur <bnurdemirhan@gmail.com>
2021-05-20 17:55:55 +0300

    Day2

---
+++ oop2Inheritance/src/oop2Inheritance/Customer.java
@@ -0,0 +1,9 @@
+package oop2Inheritance;
+
+public class Customer { // Base, temel sýnýf. (miras alýnacak olan sýnýf)
+	
+	int id;
+	String customerNumber;
+
+}
+
---
+++ oop2Inheritance/src/oop2Inheritance/IndividualCustomer.java
@@ -0,0 +1,11 @@
+package oop2Inheritance;
+
+//Inheritance(miras alma)
+
+public class IndividualCustomer extends Customer{ // IndividualCustomer Customer'dan miras alýr.
+		
+		String firstName;
+		String lastName;
+		String nationalIdentity;
+
+}
