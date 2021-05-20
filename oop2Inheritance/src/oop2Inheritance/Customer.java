d2bc0a0a524d95cb0add5141c4faf3ec220b909b
Betül Nur <bnurdemirhan@gmail.com>
2021-05-20 17:53:55 +0300

    edited

--- oop2Inheritance/src/oop2Inheritance/Customer.java
+++
@@ -1,9 +0,0 @@
-package oop2Inheritance;
-
-public class Customer { // Base, temel sýnýf. (miras alýnacak olan sýnýf)
-	
-	int id;
-	String customerNumber;
-
-}
-
--- oop2Inheritance/src/oop2Inheritance/UnionCustomer.java
+++
@@ -1,9 +0,0 @@
-package oop2Inheritance;
-
-//Inheritance(miras alma)
-
-
-public class UnionCustomer extends Customer{  // UnionCustomer Customer'dan miras alýr.
-	
-	String unionName;
-}
